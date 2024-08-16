efb77ed933a2403e74e3282907d79d0c1a6056d1
bharathane <mandadurgabharath@gmail.com>
2024-08-16 09:49:02 +0530

    add logout popup

--- src/assets/HeadNav/index.tsx
+++ src/assets/HeadNav/index.tsx
@@ -1,10 +1,17 @@
 import { FcMenu } from "react-icons/fc";
 import Cookies from "js-cookie";
 import "./index.css";
+import { useState } from "react";
 import AddPopup from "../AddPopup";
 import AppContext from "../../context/appContext";
 import { IoCloseCircleOutline } from "react-icons/io5";
+import Modal from "react-modal";
+Modal.setAppElement("#root");
 const HeadNav = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
   const logoutFromUi = () => {
     Cookies.remove("jwt_token");
     localStorage.removeItem("username");
@@ -24,10 +31,49 @@ const HeadNav = () => {
                 type="button"
                 className="logout-button"
                 style={{ width: "6vw" }}
-                onClick={logoutFromUi}
+                onClick={openModal}
               >
                 logout
               </button>
+              <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={{
+                  content: {
+                    top: "50%",
+                    left: "50%",
+                    right: "auto",
+                    bottom: "auto",
+                    marginRight: "-50%",
+                    transform: "translate(-50%, -50%)",
+                    padding: "20px",
+                    backgroundColor: "white",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)", // Shadow to pop the modal
+                    borderRadius: "10px", // Rounded corners
+                  },
+                  overlay: {
+                    backgroundColor: "rgba(0, 0, 0, 0.5)", // Dark overlay for focus on popup
+                  },
+                }}
+              >
+                <h3>Are you sure! you want to logout...</h3>
+                <button
+                  type="button"
+                  className="logout-button"
+                  style={{ width: "6vw" }}
+                  onClick={logoutFromUi}
+                >
+                  YES
+                </button>
+
+                <button
+                  onClick={closeModal}
+                  type="button"
+                  className="logout-button"
+                >
+                  NO
+                </button>
+              </Modal>
               <AddPopup />
             </div>
             <button
